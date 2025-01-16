66c286d3cf9d8916bf15c2e5cd9ad57d6f11da60
carlo <kirollosedhom@gmail.com>
2025-01-16 14:00:52 +0200

    recursion to flatten each element if array*

---
+++ flatten_array.js
@@ -0,0 +1,47 @@
+
+/* 
+Scrimba mascot Pumpkin has won the grand prize at an international 
+cat show. Below are Pumpkin's scores from the judges, as well as all the 
+prizes he's won. In all the excitement of victory,
+they've become a jumbled mess of nested arrays. Let's 
+help Pumpkin by sorting it out. 
+
+Write a function to flatten nested arrays of strings or
+numbers into a single array. There's a method
+for this, but pratice both doing it manually and using the method. 
+
+Example input: [1, [4,5], [4,7,6,4], 3, 5]
+Example output: [1, 4, 5, 4, 7, 6, 4, 3, 5]
+*/
+
+const kittyScores = [
+    [39, 99,[1,2,3], 76], 89, 98, [87, 56, 90], 
+    [96, 95], 40, 78, 50, [63]
+];
+
+const kittyPrizes = [
+    ["💰", "🐟", "🐟"], "🏆", "💐", "💵", ["💵", "🏆"],
+    ["🐟","💐", "💐"], "💵", "💵", ["🐟"], "🐟"
+];
+
+// function flatten(arr){
+//     return arr.flat()
+// }
+
+// alternative way
+
+function flatten(arr){
+    let flattenedArr = []
+    arr.forEach(element => {
+        if (Array.isArray(element)){
+            flattenedArr = [...flattenedArr, ...flatten(element)]
+        }
+        else{
+            flattenedArr.push(element)
+        }
+    });
+    return flattenedArr
+    
+}
+console.log(flatten(kittyPrizes));
+console.log(flatten(kittyScores));
